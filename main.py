f3a668dc399eee8f4b85dbf82896837e3c6b4d1a
vilavivida <chuhany2@illinois.edu>
2020-05-15 16:19:49 -0500

    Upload main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from generate import get_agreement_across_adjectives
+from generate import get_agreement_across_PP
+from generate import get_agreement_across_RC
+
+file_name_1 = "nouns.txt"
+file_name_2 = "adjectives.txt"
+file_name_3 = "prepositions.txt"
+file_name_4 = "pronouns.txt"
+file_name_5 = "pronoun_third_person.txt"
+input_directory = "word_lists/"
+
+start_words = ['this', 'these', 'that', 'those']
+mask = "[MASK]"
+
+#open and read word_lists
+with open(input_directory + file_name_1) as f:
+    nouns_list = f.read().lower().split("\n")
+
+with open(input_directory + file_name_2) as f:
+    adjectives_list = f.read().lower().split("\n")
+
+with open(input_directory + file_name_3) as f:
+    prepositions_list = f.read().lower().split("\n")
+
+with open(input_directory + file_name_4) as f:
+    pronouns_list = f.read().lower().split("\n")
+
+with open(input_directory + file_name_5) as f:
+    pronouns_third_person_list = f.read().lower().split("\n")
+
+get_agreement_across_adjectives(start_words, adjectives_list)
+get_agreement_across_PP(prepositions_list, nouns_list, adjectives_list)
+get_agreement_across_RC(nouns_list, pronouns_list, adjectives_list, pronouns_third_person_list)
