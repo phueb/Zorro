047a4200aa720433f38c55666df9e12b3c1c44de
vilavivida <chuhany2@illinois.edu>
2020-05-21 01:30:01 -0500

    Update code/output

---
+++ babeval/agreement_in_question/generate4.py
@@ -0,0 +1,6 @@
+def get_agreement_in_question(nouns_list):
+    for noun in nouns_list:
+        sentence_1 = "where" + " " + "[MASK]" + " " + "the" + " " + noun + " " + "go" + " " + "?"
+        print(sentence_1, file = open("agreement_in_question.txt","a"))
+        sentence_2 = "where" + " " + "[MASK]" + " " + "the" + " " + noun + " " + "?"
+        print(sentence_2, file = open("agreement_in_question.txt","a"))
