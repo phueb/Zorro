b4450c68374aa61a89e865ed6fe88a350efcbe63
vilavivida <chuhany2@illinois.edu>
2020-06-22 23:26:57 -0500

    Improve Visualizer to plot results from multiple models

--- babeval/visualizer.py
+++ babeval/visualizer.py
@@ -1,5 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
 
 class Visualizer:
     def __init__(self, dpi=192):
@@ -35,34 +37,46 @@ class Visualizer:
 
         plt.show()
 
-    def make_barplot_for_multiple_models(self, x_tick_labels, average_dict, std_dict):
+    def make_barplot_for_multiple_models(self, x_tick_labels, average_dict, std_dict, prediction_file_names):
 
-        x = np.arange(len(x_tick_labels))
+        #single bar
+        num_models = 5
         width = 0.2
+        x = np.arange(len(x_tick_labels))
+
         num_axes = sum([len(i) for i in average_dict.values()])
+        fig, axs = plt.subplots(num_axes, sharex=True, sharey=True, gridspec_kw={'hspace': 0}, dpi=self.dpi)
 
-        fig, axs = plt.subplots(num_axes, sharex='all', sharey='all', dpi=self.dpi)
-        if num_axes == 1:
-            axs = [axs]
+        fig.suptitle(f'Multiple BERTs Scoring \n Scoring {len(prediction_file_names)} Models', fontsize=8)
 
-        #ax_title = "average"
-        for ax, ax_title in zip(axs, average_dict.keys()):
-            ax.set_xticks(x + width)
-            ax.set_xticklabels(x_tick_labels)
-            ax.set_ylabel('Proportion')
+        #decompose dict
+        avg_lst = []
+        std_lst = []
+        label_lst = []
+        colors = [f'C{i}' for i in range(num_models)]
 
-            file_name2props = average_dict[ax_title]
-            num_models = len(file_name2props)
-            edges = [width * i for i in range(num_models)]
-            colors = [f'C{i}' for i in range(num_models)]
-            std = std_dict["standard deviation"].values()
+        for avg_title, std_title in zip(average_dict, std_dict):
+            average_value = average_dict[avg_title].values()
+            std_value = std_dict[std_title].values()
+            label_dic = average_dict[avg_title].keys()
 
+            for avg, std, label in zip(average_value, std_value, label_dic):
+                avg_lst.append(avg)
+                std_lst.append(std)
+                label_lst.append(label)
 
-            for edge, color, number, std_lst in zip(edges, colors, file_name2props.keys(), std):
-                ax.bar(x + edge, file_name2props[number], width, yerr = std_lst, color=color)
-                ax.set_title(ax_title, fontweight="bold", size=8)
+        #plotting bars
+        for i, avg, std, label in zip(range(num_axes), avg_lst, std_lst, label_lst):
+            axs[i].set_xticks(x)
+            axs[i].set_xticklabels(x_tick_labels)
+            axs[i].set_ylabel('Proportion')
 
-        # # legend
-        # plt.legend(prop={'size': 8}, bbox_to_anchor=(0.0, -0.5), loc='upper left', frameon=False)
+            axs[i].bar(x, avg, width, yerr = std, color = colors)
+            axs[i].set_title(label, fontweight="bold", size=8)
+
+        # Hide x labels and tick labels for all but bottom plot.
+        for ax in axs:
+            ax.label_outer()
 
         plt.show()
+
