8f7528483e36051c79a09854d87ea98a3fb8f321
vilavivida <chuhany2@illinois.edu>
2020-07-15 19:28:08 -0500

    Split generate.py into two separate scripts

---
+++ babeval/agreement_in_1_verb_question/generate.py
@@ -0,0 +1,28 @@
+import random
+from pathlib import Path
+
+from babeval.vocab import get_vocab
+
+NUM_NOUNS_FROM_EACH_LIST = 400  # there are only 414 plurals
+
+template1 = 'where [MASK] the {} ?'
+template2 = 'what [MASK] the {} ?'
+
+
+nouns_plural = (Path(__file__).parent / 'word_lists' / 'nouns_plural_annotator2.txt').open().read().split()
+nouns_plural = [w for w in nouns_plural if w in get_vocab()]
+
+nouns_singular = (Path(__file__).parent / 'word_lists' / 'nouns_singular_annotator2.txt').open().read().split()
+nouns_singular = [w for w in nouns_singular if w in get_vocab()]
+
+
+def main():
+    random.seed(2)
+
+    nouns_balanced = random.sample(nouns_singular, k=NUM_NOUNS_FROM_EACH_LIST) + \
+                     random.sample(nouns_plural, k=NUM_NOUNS_FROM_EACH_LIST)
+
+    for noun in nouns_balanced:
+        yield template1.format(noun)
+        yield template2.format(noun)
+
