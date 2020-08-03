c8066481cc9081ac85d1db04638008adbc91498d
phueb <info@philhuebner.com>
2020-08-03 09:08:54 -0500

    remove title-cased nouns from test sentences

--- babeval/agreement_across_adjectives_2/generate.py
+++ babeval/agreement_across_adjectives_2/generate.py
@@ -10,8 +10,6 @@ Look at [MASK] fighting toy .
 
 template = 'look at [MASK] {} {} .'
 
-# start_words = ['this', 'these', 'that', 'those']
-
 adjectives_list = (Path(__file__).parent / 'word_lists' / 'adjectives_annotator1.txt').open().read().split()
 adjectives_list = [w for w in adjectives_list if w in get_vocab()]
 
@@ -33,7 +31,6 @@ def main():
         al1 = random.sample(adjectives_list, k=len(adjectives_list))
         al2 = random.sample(adjectives_list, k=len(adjectives_list))
         al3 = random.sample(adjectives_list, k=len(adjectives_list))
-
 
         for adj1, adj2, adj3 in zip(al1, al2, al3):
             yield template.format(' '.join([adj1]), noun)
--- make_sentences.py
+++ make_sentences.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from babeval.agreement_across_adjectives.generate import main as generate_agreement_across_adjectives
+from babeval.agreement_across_adjectives_2.generate import main as generate_agreement_across_adjectives_2
 from babeval.agreement_across_PP.generate import main as generate_agreement_across_pp
 from babeval.agreement_across_RC.generate import main as generate_agreement_across_rc
 from babeval.agreement_in_1_verb_question.generate import main as generate_agreement_in_1_verb_question
@@ -17,6 +18,12 @@ with open(out_path, 'w') as f:
         f.write(sentence + '\n')
     print(f'Saved {n:,} sentences to {out_path}')
 
+# agreement_across_adjectives_2
+out_path = output_folder / 'agreement_across_adjectives_2.txt'
+with open(out_path, 'w') as f:
+    for n, sentence in enumerate(generate_agreement_across_adjectives_2()):
+        f.write(sentence + '\n')
+    print(f'Saved {n:,} sentences to {out_path}')
 
 # agreement_across_PP
 out_path = output_folder / 'agreement_across_PP.txt'
@@ -25,14 +32,12 @@ with open(out_path, 'w') as f:
         f.write(sentence + '\n')
     print(f'Saved {n:,} sentences to {out_path}')
 
-
 # agreement_across_RC
 out_path = output_folder / 'agreement_across_RC.txt'
 with open(out_path, 'w') as f:
     for n, sentence in enumerate(generate_agreement_across_rc()):
         f.write(sentence + '\n')
     print(f'Saved {n:,} sentences to {out_path}')
-
 
 # generate_agreement_in_1_verb_question
 out_path = output_folder / 'agreement_in_1_verb_question.txt'
